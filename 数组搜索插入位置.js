3b3e4e6375eb5fb34513af74f725de5b2e397744
dreamITGirl <913894936@qq.com>
2020-09-15 11:01:38 +0800

    更新数组搜索插入位置的算法

--- 数组搜索插入位置.js
+++ 数组搜索插入位置.js
@@ -27,32 +27,27 @@ var searchInsert = function (list,target) {
             }
         }
     } else { //说明target的值在左边
-        for (let i = startindex ; i > 0; i--) {
-            if (list[i-1]) {
-                if (list[i]>target && target > list[i-1]) {
-                    list.splice(i,1,target)
+        for (let i = 0; i < startindex; i++) {
+            if (list[i+1]) {
+                if (list[i] < target && target <list[i+1]) {
+                    list.splice(i+1,0,target)
+                    return i+1
+                } else if (list[i] > target) {
+                    list.splice(i,0,target)
                     return i
-                } else {
-                    if (target < list[i-1]) {
-                        list.splice(i-2,0,target)
-                        return i-2
-                    }
                 }
-            } else {
-                list.unshift(target)
-                return 0
             }
         }
     }
 }
 var list = [1,3,5,6]
 
-console.log(searchInsert(list,5)) // 2
+// console.log(searchInsert(list,5)) // 2
 
-console.log(searchInsert(list,2)) // 1 
+// console.log(searchInsert(list,2)) // 1 
 
-console.log(searchInsert(list,7)) // 4
+// console.log(searchInsert(list,7)) // 4
 
-console.log(searchInsert(list,0)) // 0
+// console.log(searchInsert(list,0)) // 0
 
-console.log(searchInsert(list,-4)) //0
+// console.log(searchInsert(list,-4)) //0
